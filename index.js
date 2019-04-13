6d53b6ba9313e283741808533c46a75b53db3871
Nicolas Marini <nmarini44@gmail.com>
2019-04-13 23:07:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42
+  } else {
+    return 42 - blockNumber
+  }
+}
+
+function distanceFromHqInFeet (blockNumber) {
+   return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet (startBlock, endBlock) {
+  return startBlock < endBlock ? (endBlock - startBlock) * 264 : (startBlock - endBlock) * 264
+}
+
+function calculatesFarePrice (startBlock, endBlock) {
+  const distance = distanceTravelledInFeet(startBlock, endBlock)
+
+  if (distance > 2500) {
+    return 'cannot travel that far';
+  } else if (distance > 2000) {
+      return 25;
+  } else if (distance > 400) {
+      return (distance - 400 ) * .02;
+  } else {
+      return 0;
+    }
+}
